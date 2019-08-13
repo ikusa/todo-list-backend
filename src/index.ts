ee4acf8a35822c329eb77a931ee5a7e374ffbe55
Joshua <joshuasetiawan04@gmail.com>
2019-08-13 12:00:18 +0700

    expose todoes , createTodo, and updateTodo API

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-async function main(): Promise<void> {}
+import { prisma } from '../generated/prisma-client';
+import { GraphQLServer } from 'graphql-yoga';
+import { schema } from './schema';
+
+const server = new GraphQLServer({
+  schema,
+  context: { prisma },
+});
+
+async function main(): Promise<void> {
+  server.start(() => console.log('Server is running on http://localhost:4000'));
+}
 
 main().catch(e => console.error(e));
